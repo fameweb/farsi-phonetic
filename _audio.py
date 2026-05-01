40df0b61e12085839be145db97c43f05902a9b61
Jeremy Fama <jeremy@purecobalt.com>
2026-04-30 23:52:25 -0400

    Remove turkish for test

--- _audio.py
+++ _audio.py
@@ -20,7 +20,7 @@ def text_to_speech(text):
     data = {
     "text": text,
     "model_id": "eleven_multilingual_v2",
-    "language_code": "tr", # For some reason 'fa' isn't supported, but 'tr' (Turkish) is close enough for Farsi phonetics
+    # "language_code": "tr", # For some reason 'fa' isn't supported, but 'tr' (Turkish) is close enough for Farsi phonetics
     "voice_settings": {
         "stability": 0.5,
         "similarity_boost": 0.8,
